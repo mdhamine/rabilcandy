859d19509f710c6e5499ae93dd2b857e12f366e7
sabinbaniya <dummymail@tutanota.de>
2023-12-12 22:59:57 +0545

    update db url

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     DB_URL:
-      "mongodb+srv://polok:d2zBoAn2O2VAFgvD@cluster0.qjrhokc.mongodb.net/ecom?retryWrites=true&w=majority",
+      "mongodb+srv://test:mangotest@cluster0.wk291zy.mongodb.net/ecom?retryWrites=true&w=majority",
     SENDER_PASSWORD: "q8YqUvVDmG4Rsdb7YQ",
     SENDER_EMAIL: "lionel61@ethereal.email",
     RECEIVER_EMAIL: "lionel61@ethereal.email",
